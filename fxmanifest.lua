d9d76271dae795155b51005632864edb3b0c082c
M4h45amu7x <lovelovethekiss123@gmail.com>
2023-03-30 18:04:43 +0700

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+version '1.0.0'
+author 'M4h45amu7x <https://www.mahasamut.xyz>'
+description 'N4Yz Splash Screen'
+repository 'https://github.com/M4h45amu7x/n4yz-splash-screen'
+
+files {
+    'index.html'
+}
+
+loadscreen 'index.html'
+
+fx_version 'cerulean'
+game 'gta5'
